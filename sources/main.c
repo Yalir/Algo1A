222766192075a5b4306db2d08acd359bdea52030
Ceylo <soltic.lucas@gmail.com>
2011-12-10 18:54:25 +0100

    Import initial.

---
+++ sources/main.c
@@ -0,0 +1,22 @@
+
+
+#include <stdio.h>
+#include "affichage.h"
+
+/** @brief Breve description de la fonction.
+ *
+ * Description de la fonction (but, resultats).
+ * @param par1  description du parametre.
+ * @param par2  description du parametre.
+ * @return	description du resultat retourne.
+ */
+
+
+int main (int argc, const char * argv[])
+{
+	construire_systeme();
+	traiter_systeme();
+	afficher_solutions();
+    return 0;
+}
+
