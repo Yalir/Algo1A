632a9baf7369187bc91029811a3227c2e2ab57ed
Ceylo <soltic.lucas@gmail.com>
2011-12-10 18:28:04 +0100

    Import initial et définition en pseudo code de la fonction afficher_solution().

---
+++ sources/affichage.c
@@ -0,0 +1,30 @@
+//
+//  affichage.c
+//  Algo1A
+//
+//  Created by Ceylo on 10/12/11.
+//  Copyright 2011 Yalir. All rights reserved.
+//
+
+#include "affichage.h"
+#include "donnees.h"
+
+void afficher_solution(void)
+{
+	/*
+	soit S l’ensemble des solutions
+	soit p <- S soit i un entier = 1
+	
+	if (S non nul)
+	{
+		while (p non nul)
+		{
+			afficher “Xi =” + en_texte(p->terme_droit.) i = i+1 p = p->suivant
+		}
+	}
+	else
+	{
+		afficher “Système insoluble”
+	}
+	 */
+}
---
+++ sources/affichage.h
@@ -0,0 +1,34 @@
+/**
+ * ESIL (Ecole Superieure d'Ingenieurs de Luminy, Marseille)
+ * Case 925 - 163, avenue de Luminy
+ * 13288 Marseille CEDEX 9
+ *
+ * Ce fichier est l'oeuvre d'eleves de l'ESIL. Il ne peut etre
+ * reproduit, utilise ou modifie sans l'avis express de ses auteurs.
+ */ 
+
+/**
+ * @author NAJEM Ali <ali.najem@etumel.esil.univmed.fr>
+ * @author SOLTIC Lucas <lucas.soltic@etumel.esil.univmed.fr>
+ *
+ * @version 0.0.1 / 10-12-2011
+ * @todo
+ * @bug
+ */
+
+
+/**
+ * @file affichage.h
+ * @brief Contient les fonctions permettant l'affichage du systeme et des
+ * solutions
+ *
+ * Description des objectifs du programme, le sujet a traiter.
+ * Une trace d'execution.
+ */
+
+#ifndef AFFICHAGE_H
+#define AFFICHAGE_H
+
+void afficher_solution(void);
+
+#endif
