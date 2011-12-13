91b612c3b4267648b00291c582d42ef6865bf21f
Ceylo <soltic.lucas@gmail.com>
2011-12-13 17:04:54 +0100

    Import initial des modules de construction et résolution de système.

---
+++ sources/construction.h
@@ -0,0 +1,35 @@
+/**
+ * ESIL (Ecole Superieure d'Ingenieurs de Luminy, Marseille)
+ * Case 925 - 163, avenue de Luminy
+ * 13288 Marseille CEDEX 9
+ *
+ * Ce fichier est l'oeuvre d'eleves de l'ESIL. Il ne peut etre
+ * reproduit, utilise ou modifie sans l'avis express de ses auteurs.
+ */ 
+
+/**
+ * @author NAJEM Ali <ali.najem@etumel.esil.univmed.fr>
+ * @author SOLTIC Lucas <lucas.soltic@etumel.esil.univmed.fr>
+ *
+ * @version 0.0.1 / 13-12-2011
+ * @todo
+ * @bug
+ */
+
+
+/**
+ * @file construction.h
+ * @brief Contient les fonctions permettant de construire la structure du
+ * système à résoudre
+ *
+ * Description des objectifs du programme, le sujet a traiter.
+ * Une trace d'execution.
+ */
+
+#ifndef CONSTRUCTION_H
+#define CONSTRUCTION_H
+
+void construire_systeme(void);
+
+#endif
+
---
+++ sources/resolution.h
@@ -0,0 +1,34 @@
+/**
+ * ESIL (Ecole Superieure d'Ingenieurs de Luminy, Marseille)
+ * Case 925 - 163, avenue de Luminy
+ * 13288 Marseille CEDEX 9
+ *
+ * Ce fichier est l'oeuvre d'eleves de l'ESIL. Il ne peut etre
+ * reproduit, utilise ou modifie sans l'avis express de ses auteurs.
+ */ 
+
+/**
+ * @author NAJEM Ali <ali.najem@etumel.esil.univmed.fr>
+ * @author SOLTIC Lucas <lucas.soltic@etumel.esil.univmed.fr>
+ *
+ * @version 0.0.1 / 13-12-2011
+ * @todo
+ * @bug
+ */
+
+
+/**
+ * @file resolution.h
+ * @brief Contient les fonctions permettant de résoudre le système
+ *
+ * Description des objectifs du programme, le sujet a traiter.
+ * Une trace d'execution.
+ */
+
+#ifndef RESOLUTION_H
+#define RESOLUTION_H
+
+void traiter_systeme(void);
+
+#endif
+
