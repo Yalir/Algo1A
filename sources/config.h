df414d85232a85f5a8c31a521d884ad8e3cfea5b
Ali NAJEM <ali.najem@hotmail.fr>
2012-01-03 15:08:01 +0100

    Import initial. Definition de la macro WINDOWS sous Windows.

---
+++ sources/config.h
@@ -0,0 +1,27 @@
+/**
+ * ESIL (Ecole Superieure d'Ingenieurs de Luminy, Marseille)
+ * Case 925 - 163, avenue de Luminy
+ * 13288 Marseille CEDEX 9
+ *
+ * Ce fichier est l'oeuvre d'eleves de l'ESIL. Il ne peut etre
+ * reproduit, utilise ou modifie sans l'avis express de ses auteurs.
+ */ 
+
+/**
+ * @author NAJEM Ali <ali.najem@etumel.esil.univmed.fr>
+ * @author SOLTIC Lucas <lucas.soltic@etumel.esil.univmed.fr>
+ *
+ * @version 0.0.1 / 10-12-2011
+ * @todo
+ * @bug
+ */
+ 
+ #ifndef CONFIG_H
+ #define CONFIG_H
+ 
+ #if defined(_WIN32) || defined(__WIN32__)
+ #define WINDOWS 1
+ #endif
+ 
+ #endif
+ 
