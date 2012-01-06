e920f798ede82c46804ae9cbc1d35e5e0b11938c
Ceylo <soltic.lucas@gmail.com>
2012-01-06 18:33:18 +0100

    Ajouté commentaires Doxygen.

--- sources/donnees.h
+++ sources/donnees.h
@@ -33,14 +33,67 @@
 extern Solutions s;
 extern Systeme t;
 
+/** @brief Créé un nouveau système vide initialisé
+ * En cas d'erreur lors de la création, le programme est stoppé
+ *
+ * @return Le nouveau système
+ */
 Systeme creer_systeme(void);
+
+
+/** @brief Créé un nouveau terme vide initialisé
+ * En cas d'erreur lors de la création, le programme est stoppé
+ *
+ * @return Le nouveau terme
+ */
 Terme creer_terme(void);
+
+
+/** @brief Créé un nouvel argument vide initialisé
+ * En cas d'erreur lors de la création, le programme est stoppé
+ *
+ * @return Le nouvel argument
+ */
 Argument creer_argument(void);
+
+
+/** @brief Créé une nouvelle solution vide initialisée
+ * En cas d'erreur lors de la création, le programme est stoppé
+ *
+ * @return La nouvelle solution
+ */
 Solutions creer_solutions(void);
 
+
+/** @brief Détruit récursivement un système en libérant la mémoire allourée
+ * @a sys doit être un système valide (non nul)
+ *
+ * @param sys le système à détruire
+ */
 void destroy_systeme(Systeme sys);
+
+
+/** @brief Détruit récursivement un terme en libérant la mémoire allourée
+ * @a terme doit être un terme valide (non nul)
+ *
+ * @param terme le terme à détruire
+ */
 void destroy_terme(Terme terme);
+
+
+/** @brief Détruit récursivement un argument en libérant la mémoire allourée
+ * @a arg doit être un argument valide (non nul)
+ *
+ * @param arg l'argument à détruire
+ */
 void destroy_argument(Argument arg);
+
+
+/** @brief Détruit récursivement une solution en libérant la mémoire allourée
+ * @a sol doit être une solution valide (non nul)
+ *
+ * @param sol la solution à détruire
+ */
 void destroy_solutions(Solutions sol);
 
 #endif
