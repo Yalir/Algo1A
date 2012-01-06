a0e014d25b9653642cbfcab7f8798ee70fbaa46e
Ali NAJEM <ali.najem@hotmail.fr>
2012-01-06 17:22:47 -0500

    implementé la fonction construire_systeme()

--- sources/construction.c
+++ sources/construction.c
@@ -6,11 +6,76 @@
  * Ce fichier est l'oeuvre d'eleves de l'ESIL. Il ne peut etre
  * reproduit, utilise ou modifie sans l'avis express de ses auteurs.
  */ 
- 
- #include "construction.h"
- 
- void construire_systeme(void)
- {
- 	
- }
- 
+
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h> 
+#include "Types.h"
+#include "construction.h"
+#include "donnees.h"
+
+void construire_systeme(void)
+{
+	t = creer_systeme();
+	
+	// construire le premier systeme
+	// X3 = 2 
+	
+	t->terme_gauche= creer_terme();
+	t->terme_gauche->type_terme = Variable;
+	t->terme_gauche->contenu_terme.val = 2;
+	
+	t->terme_droit = creer_terme();
+	t->terme_droit->type_terme = Constante;
+	t->terme_droit->contenu_terme.val= 2;
+	
+	// F1(X1,X2) = F1(F2(1,X2),X3)
+	t->suivant = creer_systeme();
+	
+	// F1(X1,X2)
+	t->suivant->terme_gauche = creer_terme(); 
+	t->suivant->terme_gauche->type_terme = Fonction+1;
+	t->suivant->terme_gauche->contenu_terme.arguments = creer_argument();
+	t->suivant->terme_gauche->contenu_terme.arguments->terme_argument =creer_terme(); 
+	t->suivant->terme_gauche->contenu_terme.arguments->terme_argument->type_terme = Variable;
+	t->suivant->terme_gauche->contenu_terme.arguments->terme_argument->contenu_terme.val = 1; // X1
+	
+	
+	t->suivant->terme_gauche->contenu_terme.arguments->suivant = creer_argument();
+	
+	// X2
+	t->suivant->terme_gauche->contenu_terme.arguments->suivant->terme_argument=creer_terme(); 
+	t->suivant->terme_gauche->contenu_terme.arguments->suivant->terme_argument->type_terme = Variable;
+	t->suivant->terme_gauche->contenu_terme.arguments->suivant->terme_argument->contenu_terme.val= 2;
+	
+	
+	// F1(F2(1,X2),X3)
+	
+	t->suivant->terme_droit = creer_terme(); 
+	t->suivant->terme_droit->type_terme = Fonction+1;
+	
+	t->suivant->terme_droit->contenu_terme.arguments=creer_argument();
+	t->suivant->terme_droit->contenu_terme.arguments->terme_argument=creer_terme();
+	t->suivant->terme_droit->contenu_terme.arguments->terme_argument->type_terme = Fonction+2;
+	
+	
+	t->suivant->terme_droit->contenu_terme.arguments->terme_argument->contenu_terme.arguments=creer_argument();
+	t->suivant->terme_droit->contenu_terme.arguments->terme_argument->contenu_terme.arguments->terme_argument=creer_terme();
+	t->suivant->terme_droit->contenu_terme.arguments->terme_argument->contenu_terme.arguments->terme_argument->type_terme= Constante; 
+	t->suivant->terme_droit->contenu_terme.arguments->terme_argument->contenu_terme.arguments->terme_argument->contenu_terme.val=1 ; //F1(F2(1),    	   	 
+	
+	
+	t->suivant->terme_droit->contenu_terme.arguments->terme_argument->contenu_terme.arguments->suivant=creer_argument();
+	
+	t->suivant->terme_droit->contenu_terme.arguments->terme_argument->contenu_terme.arguments->suivant->terme_argument=creer_terme();
+	
+	t->suivant->terme_droit->contenu_terme.arguments->terme_argument->contenu_terme.arguments->suivant->terme_argument->type_terme= Variable; 
+	t->suivant->terme_droit->contenu_terme.arguments->terme_argument->contenu_terme.arguments->suivant->terme_argument->contenu_terme.val=2 ; //F1(F2(1,X2)
+	
+	t->suivant->terme_droit->contenu_terme.arguments->suivant = creer_argument();
+	t->suivant->terme_droit->contenu_terme.arguments->suivant->terme_argument=creer_terme();
+	t->suivant->terme_droit->contenu_terme.arguments->suivant->terme_argument->type_terme=Variable;
+	
+	//F1(F2(1,X2),X3)
+	t->suivant->terme_droit->contenu_terme.arguments->suivant->terme_argument->contenu_terme.val=3; 
+}
