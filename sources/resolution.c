1b8e2f9451977295c8a0bfa05a08c6db80c36c70
Ceylo <soltic.lucas@gmail.com>
2012-01-13 13:55:39 +0100

    Ajouté la structure du code pour gérer les cas manquants.

--- sources/resolution.c
+++ sources/resolution.c
@@ -34,18 +34,22 @@ void traiter_equation(Solutions s, Equation *e, Equation **dansSys, Equation **d
 	Terme ps = e->terme_gauche;
 	Terme pt = e->terme_droit;
 	
+	// s et t variables
 	if (ps->type_terme == Variable && pt->type_terme == Variable)
 	{
+		// même variable
 		if (ps->contenu_terme.val == pt->contenu_terme.val) // si indice de s = indice de t alors
 		{
 			// rien à faire, on supprimera cette équation
 		}
 		else
 		{
+			// X3 = X4
 			if (ps->contenu_terme.val < pt->contenu_terme.val) // si indice de s < indice de t alors
 			{
 				*dansSolu = e; // on rangera l’éq. dans S
 			}
+			// X4 = X3
 			else
 			{
 				// intervertir s et t
@@ -56,15 +60,38 @@ void traiter_equation(Solutions s, Equation *e, Equation **dansSys, Equation **d
 			}
 		}
 	}
+	// s variable et t constante
 	else if (ps->type_terme == Variable && pt->type_terme == Constante)
 	{
 		*dansSolu = e; // on rangera l’éq. dans S
 	}
+#warning todo
 	else if (ps->type_terme == Variable && pt->type_terme >= Fonction) // && (t est une fonction dépendant de S)
 	{
 		// S <- insoluble
 		destroy_solutions(s);
 		s = NULL;
+	}
+	else if (ps->type_terme != Variable && pt->type_terme == Variable)
+	{
+		// intervertir s et t
+	}
+	else if (ps->type_terme != Variable && pt->type_terme != Variable)
+	{
+		if (ps->type_terme == Constante && pt->type_terme == Constante && ps->contenu_terme.val == pt->contenu_terme.val)
+		{
+			// ignorer l'équation
+		}
+#warning todo
+		else if (ps->type_terme >= Fonction && pt->type_terme >= Fonction /* et même nb de params */)
+		{
+			// décapsuler les fonctions et faire les correspondances
+		}
+		else
+		{
+			destroy_solutions(s);
+			s = NULL;
+		}
 	}
 	else
 	{
