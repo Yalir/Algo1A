7234b1f802427581201df9488e1c2511343f0570
Ali NAJEM <ali.najem@hotmail.fr>
2012-01-16 17:16:31 -0500

    Correction sur la fonction obtenir_representant_terme.

--- sources/representant.c
+++ sources/representant.c
@@ -16,30 +16,27 @@
 Terme obtenir_representant_terme(const Terme u, const Systeme s)
 {
 	int trouver=0;
-	Terme nouveau_terme;
+	Terme nouveau_terme=NULL;
 	Argument arg;
 	Equation* parcours_solution;
 	
 	assert(u !=NULL);
-	
-	nouveau_terme = creer_terme();
 
 	if(u->type_terme == Variable)
-	{		
-		parcours_solution = creer_equation();
-		parcours_solution = (Equation*)s; // associer s à parcours_solution
+	{
+		parcours_solution = copie_equation((Equation*)s); // associer s à parcours_solution
 	
 		// si (S contient une équation e2 de la forme u = v alors) 
 
-		while (s!=NULL && !trouver) 
+		while (parcours_solution != NULL && !trouver) 
 		{			
 			if(((parcours_solution->terme_gauche->contenu_terme.val) == (u->contenu_terme.val)) && ((parcours_solution->terme_gauche->contenu_terme.val) == (u->contenu_terme.val)) && ((parcours_solution->terme_gauche->type_terme) == (u->type_terme)))
 			{
-				// on affecte v à u			
-				nouveau_terme= parcours_solution->terme_droit;
 				trouver = 1;
+				// on affecte v à u			
+				nouveau_terme = copie_terme(parcours_solution->terme_droit);
 				// on affecte le representant de v au nouveau_terme
-				nouveau_terme = obtenir_representant_terme(u,s);
+				nouveau_terme = obtenir_representant_terme(nouveau_terme,s);
 			}
 			parcours_solution = parcours_solution->suivant ; // passer à la solution suivante
 		}
@@ -47,14 +44,13 @@ Terme obtenir_representant_terme(const Terme u, const Systeme s)
 	else
 	{
 		// on affecte le terme u au nouveau_terme dans le cas où u est une constante ou une fonction
-		nouveau_terme = u;
+		nouveau_terme = copie_terme(u);
 		
-		if(u->type_terme == Fonction)
+		if(nouveau_terme->type_terme == Fonction)
 		{
 			// u est de la forme :  fm(q1,q2,...,qn)
 			// Le representant de u dans S est fm(obtenir_representant (q1), obtenir_representant(q2), ..., obtenir_representant(qn))
-			arg = creer_argument();	
-			arg = u->contenu_terme.arguments;
+			arg = nouveau_terme->contenu_terme.arguments;
 			
 			// boucle qui parcour tout les arguments de la fonction fm 
 			while (arg != NULL)
@@ -67,8 +63,9 @@ Terme obtenir_representant_terme(const Terme u, const Systeme s)
 		}
 		// si jamais u->type_terme n'est pas une fonction, le représentant de u dans S est lui-même (u est une constante)
 	}
+	
 	return nouveau_terme ;
-} 
+}
 
 Equation* obtenir_representant_equation(const Equation *e, const Systeme s)
 {
