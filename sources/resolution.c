bf53f2213fd3ca969d24490e5df13d0c55a78b08
Ceylo <soltic.lucas@gmail.com>
2012-01-16 16:51:09 +0100

    Implémenté traiter_systeme(). Ajouté traiter_equation(), contient_terme(), decapsuler_fonctions(), compter_arguments() et ranger_solutions(). Supprimé obtenir_representant() et obtenir_tout_les_representants().

--- sources/resolution.c
+++ sources/resolution.c
@@ -7,23 +7,66 @@
  * reproduit, utilise ou modifie sans l'avis express de ses auteurs.
  */ 
  
- #include "resolution.h"
- #include "donnees.h"
- #include "Types.h"
- #include <stdlib.h>
- #include <assert.h>
- 
+#include "resolution.h"
+#include "donnees.h"
+#include "representant.h"
+#include "Types.h"
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+
 Solutions traiter_systeme(Systeme sys)
 {
-	return NULL;
+	assert(sys != NULL);
+	
+	Solutions s = creer_solutions();
+	initialiser_solutions(s, 5);
+	
+	Equation *e = sys;
+	int insoluble = 0;
+	
+	while (s != NULL && insoluble == 0)
+	{
+		Equation *representee = obtenir_representant_equation(e, sys);
+		Equation *dansSys = NULL;
+		Equation *dansSolu = NULL;
+		
+		insoluble = traiter_equation(s, representee, &dansSys, &dansSolu);
+		
+		// s'il y a des équations à ajouter au système d'équations
+		if (dansSys)
+		{
+			Equation *current = sys;
+			
+			// aller au dernier maillon du système
+			while (current->suivant)
+				current = current->suivant;
+			
+			// rajouter la liste d'équation au système
+			current->suivant = dansSys;
+		}
+		
+		// s'il y a des équations à ajouter au système de solutions
+		if (dansSolu)
+		{
+			// rajouter la liste des termes droit au système
+			ranger_solutions(s, dansSolu);
+		}
+		
+		destroy_equation(representee);
+		e = e->suivant;
+	}
+	
+	return s;
 }
  
 // après l’exécution de la fonction, dansSys contient l’équation qui 
 // doit être rajoutée au système, et dansSolu l’équation qui doit
 // être rajoutée aux solutions. Si l’un ou les deux paramètres
 // contiennent ‘rien’, il n’y a pas à les considérer
-void traiter_equation(Solutions s, Equation *e, Equation **dansSys, Equation **dansSolu)
+int traiter_equation(const Solutions s, const Equation *e, Equation **dansSys, Equation **dansSolu)
 {
+	assert(s != NULL);
 	assert(e != NULL);
 	assert(dansSys != NULL);
 	assert(dansSolu != NULL);
@@ -31,121 +74,246 @@ void traiter_equation(Solutions s, Equation *e, Equation **dansSys, Equation **d
 	*dansSys = NULL;
 	*dansSolu = NULL;
 	
-	Terme ps = e->terme_gauche;
-	Terme pt = e->terme_droit;
+	int flag = 0;
+	Terme t_gauche = e->terme_gauche;
+	Terme t_droit = e->terme_droit;
 	
 	// s et t variables
-	if (ps->type_terme == Variable && pt->type_terme == Variable)
+	if (t_gauche->type_terme == Variable && t_droit->type_terme == Variable)
 	{
 		// même variable
-		if (ps->contenu_terme.val == pt->contenu_terme.val) // si indice de s = indice de t alors
+		if (t_gauche->contenu_terme.val == t_droit->contenu_terme.val) // si indice de s = indice de t alors
 		{
 			// rien à faire, on supprimera cette équation
 		}
 		else
 		{
 			// X3 = X4
-			if (ps->contenu_terme.val < pt->contenu_terme.val) // si indice de s < indice de t alors
+			if (t_gauche->contenu_terme.val < t_droit->contenu_terme.val) // si indice de s < indice de t alors
 			{
-				*dansSolu = e; // on rangera l’éq. dans S
+				*dansSolu = copie_equation(e); // on rangera l’éq. dans S
 			}
 			// X4 = X3
 			else
 			{
-				// intervertir s et t
-				e->terme_gauche = pt;
-				e->terme_droit = ps;
 				// on réinsère l’éq inversée dans le système
-				*dansSys = e;
+				// intervertir s et t
+				*dansSys = creer_equation();
+				(*dansSys)->terme_gauche = t_droit;
+				(*dansSys)->terme_droit = t_gauche;
 			}
 		}
 	}
 	// s variable et t constante
-	else if (ps->type_terme == Variable && pt->type_terme == Constante)
+	else if (t_gauche->type_terme == Variable && t_droit->type_terme == Constante)
 	{
-		*dansSolu = e; // on rangera l’éq. dans S
+		// on rangera l’éq. dans S
+		*dansSolu = creer_equation();
+		//**dansSolu = *e;
 	}
-#warning todo
-	else if (ps->type_terme == Variable && pt->type_terme >= Fonction) // && (t est une fonction dépendant de S)
+	else if (t_gauche->type_terme == Variable && t_droit->type_terme >= Fonction)
 	{
-		// S <- insoluble
-		destroy_solutions(s);
-		s = NULL;
+		// si t est une fonction dépendant de s -> insoluble
+		if (contient_terme(t_droit, t_gauche))
+		{
+			flag = -1;
+		}
+		// sinon ranger simplement l'équation dans les solutions
+		else
+		{
+			*dansSolu = copie_equation(e); // on rangera l’éq. dans S
+		}
 	}
-	else if (ps->type_terme != Variable && pt->type_terme == Variable)
+	else if (t_gauche->type_terme != Variable && t_droit->type_terme == Variable)
 	{
 		// intervertir s et t
+		*dansSys = creer_equation();
+		(*dansSys)->terme_gauche = t_droit;
+		(*dansSys)->terme_droit = t_gauche;
 	}
-	else if (ps->type_terme != Variable && pt->type_terme != Variable)
+	else if (t_gauche->type_terme != Variable && t_droit->type_terme != Variable)
 	{
-		if (ps->type_terme == Constante && pt->type_terme == Constante && ps->contenu_terme.val == pt->contenu_terme.val)
+		if (t_gauche->type_terme == Constante && t_droit->type_terme == Constante)
 		{
-			// ignorer l'équation
+			if (t_gauche->contenu_terme.val == t_droit->contenu_terme.val)
+			{
+				// ignorer l'équation
+			}
+			else
+			{
+				// {2 = 3} -> insoluble
+				flag = -1;
+			}
 		}
-#warning todo
-		else if (ps->type_terme >= Fonction && pt->type_terme >= Fonction /* et même nb de params */)
+		else if (t_gauche->type_terme >= Fonction && t_droit->type_terme >= Fonction)
 		{
 			// décapsuler les fonctions et faire les correspondances
+			*dansSys = decapsuler_fonctions(t_gauche, t_droit); 
+			
+			if (*dansSys == NULL) // nombre différent de paramètres
+			{
+				flag = -1; // insoluble
+			}
+			
 		}
 		else
 		{
-			destroy_solutions(s);
-			s = NULL;
+			flag = -1;
 		}
 	}
 	else
 	{
-		*dansSolu = e;
+		flag = -1;
 	}
+	
+	return flag;
 }
- 
-void obtenir_representant(Terme u, Equation *e, Equation **dansSolu)
-{
-	int trouver=0;
-	assert(e != NULL);
-	u = e->terme_gauche;
-	assert(u !=NULL);
 
-	if(u->type_terme == Variable)
+int contient_terme(Terme conteneur, Terme t)
+{
+	assert(conteneur != NULL);
+	assert(t != NULL);
+	assert(t->type_terme == Variable);
+	
+	int found = 0;
+	
+	// si le terme est une variable, on vérifie si elle correspond
+	// à celle que l'on cherchait
+	if (conteneur->type_terme == Variable)
 	{
-		// si (S contient une équation e2 de la forme u = v alors) 
-		while (*dansSolu!=NULL && !trouver) 
-		{
-			assert((*dansSolu)->terme_gauche);
-			
-			if((((*dansSolu)->terme_gauche->contenu_terme.val) == (u->contenu_terme.val)) && (((*dansSolu)->terme_gauche->contenu_terme.val) == (u->contenu_terme.val)) && (((*dansSolu)->terme_gauche->type_terme) == (u->type_terme)))
-			{
-				// on affecte v à u 
-				u= (*dansSolu)->terme_droit;
-				trouver = 1;
-				obtenir_representant((*dansSolu)->terme_droit,e,dansSolu);
-			}
-		}
+		if (conteneur->contenu_terme.val == t->contenu_terme.val)
+			found = 1;
 	}
-	else
+	// si le terme est une constante, ce n'est certainement pas la variable
+	// qu'on cherchait
+	else if (conteneur->type_terme == Constante)
 	{
-		// le représentant de u dans S est lui-même pour si u est une constante
+		// rien à faire
+	}
+	// si le terme est une fonction, on doit chercher plus en profondeur
+	else if (conteneur->type_terme >= Fonction)
+	{
+		Argument args = conteneur->contenu_terme.arguments;
 		
-		if(u->type_terme == Fonction)
+		while (args != NULL && found == 0)
 		{
-			// u est fm(q1,q2,...,qn)
-			// le representant de u dans S est fm(obtenir_representant (q1), obtenir_representant(q2), ..., obtenir_representant(qn))
-			
-			while (u->contenu_terme.arguments != NULL)
-			{
-				obtenir_representant(u->contenu_terme.arguments->terme_argument,e,dansSolu);
-				u->contenu_terme.arguments->terme_argument = u->contenu_terme.arguments->suivant;
-			}
+			found = contient_terme(args->terme_argument, t);
+			args = args->suivant;
 		}
+	}
+	
+	return found;
+}
+
+Equation *decapsuler_fonctions(Terme fgauche, Terme fdroit)
+{
+	assert(fgauche != NULL);
+	assert(fdroit != NULL);
+	assert(fgauche->type_terme >= Fonction);
+	assert(fdroit->type_terme >= Fonction);
+	
+	Equation *chaine_eq = NULL;
+	Equation *prev_eq = NULL;
+	
+	int fgauche_compte = compter_arguments(fgauche);
+	int fdroit_compte = compter_arguments(fdroit);
+	
+	// on vérifie que le nombre d'arguments correspond
+	if (fgauche_compte == fdroit_compte && fgauche_compte > 0)
+	{
+		Argument arg_gauche = fgauche->contenu_terme.arguments;
+		Argument arg_droit = fdroit->contenu_terme.arguments;
+		
+		// on traite chaque argument
+		while (arg_gauche /* && arg_droit */)
+		{
+			Equation *eq = creer_equation();
+			eq->terme_gauche = copie_terme(arg_gauche->terme_argument);
+			eq->terme_droit = copie_terme(arg_droit->terme_argument);
+			
+			// si c'est le premier maillon, on l'enregistre comme tel
+			if (chaine_eq == NULL)
+			{
+				// chaine_eq est le début de la chaîne
+				chaine_eq = eq;
+				prev_eq = chaine_eq;
+			}
+			else
+			{
+				// on fait le chaînage et on garde un pointeur sur le précédent
+				// maillon
+				prev_eq->suivant = eq;
+				prev_eq = eq;
+			}
+			
+			// puis on passe à l'argument suivant
+			arg_gauche = arg_gauche->suivant;
+			arg_droit = arg_droit->suivant;
+		}
+	}
+	
+	return chaine_eq;
+}
+
+
+int compter_arguments(Terme t)
+{
+	assert(t != NULL);
+	assert(t->type_terme >= Fonction);
+	
+	int compteur = 0;
+	Argument args = t->contenu_terme.arguments;
+	
+	while (args)
+	{
+		args = args->suivant;
+		compteur++;
+	}
+	
+	return compteur;
+}
+
+
+void ranger_solutions(Solutions s, const Equation *e)
+{
+	assert(s != NULL);
+	assert(e != NULL);
+	
+	// parcourir la liste indiquée par e
+	while (e)
+	{
+		// on vérifie qu'on a bien une variable à gauche
+		if (e->terme_gauche->type_terme == Variable)
+		{
+			int j;
+			Solutions current = s;
+			int i = e->terme_gauche->contenu_terme.val;
+			
+			// aller au bon endroit
+			for (j = 0; j < i;j++)
+				current = current->suivant;
+			
+			// on a déjà un terme_droit ?
+			if (current->terme_droit != NULL)
+			{
+				fprintf(stderr, "*** warning: erasing previously set solution\n");
+				destroy_terme(current->terme_droit);
+			}
+			
+			printf("storing solution at index %d\n", i);
+			current->terme_droit = copie_terme(e->terme_droit);
+		}
+		// sinon on ne peut pas enregistrer cette solution
+		else
+		{
+			fprintf(stderr, "*** error: trying to store equation with non variable type as left component\n");
+			abort();
+		}
+		
+		e = e->suivant;
 	}
 }
 
-void obtenir_tout_les_representants(Terme u, Systeme s, Systeme *dansSolu)
-{
-	assert(s!=NULL);
-	while (s!=NULL)
-	{
-		obtenir_representant(s->terme_gauche, s, dansSolu);
-		obtenir_representant(s->terme_droit, s, dansSolu);
-	}
-}
+
+
+ 
--- sources/resolution.h
+++ sources/resolution.h
@@ -31,14 +31,74 @@
 #include "Types.h"
 
 /** @brief Créé un système de solution à partir du système d'équations donné
+ * @a sys doit être un système valide (non nul)
+ * @a sys peut être étendu au cours du traitement
  *
- * @param sys Le système d'équations à traiter
+ * @param sys Le système d'équations à traiter (et étendre si besoin)
  * @return Le système de solutions trouvé ou NULL si le @a sys est insoluble
  */
 Solutions traiter_systeme(Systeme sys);
 
-void obtenir_representant(Terme u, Systeme e, Systeme *dansSolu);
-void obtenir_tout_les_representants(Terme u, Systeme s, Systeme *dansSolu);
+
+/** @brief Résout une équation et met à jour l'ensemble des solutions
+ * @a s et @a e doivent être des objets valides
+ * @a dansSys doit être une référence sur référence d'équation non allouée
+ * @a dansSolu doit être une référence sur référence d'équation non allouée
+ *
+ * @param s L'ensemble des solutions trouvées jusqu'à maintenant
+ * @param e L'équation à traiter
+ * @param dansSys Une référence sur une référence d'équation non allouée
+ * @param dansSolu Une référence sur une référence d'équation non allouée
+ *
+ * @return 0 si l'équation a pu être traitée, -1 si l'équation rend le système
+ * insoluble
+ */
+int traiter_equation(const Solutions s, const Equation *e, Equation **dansSys, Equation **dansSolu);
+
+
+/** @brief Vérifie si le terme @a conteneur fait référence au terme @a t
+ * @a conteneur et @a t doivent être des termes valides (non nuls) sous peine
+ * d'arrêt du programme. @a t doit être une variable.
+ *
+ * @param conteneur Le terme dans lequel on cherche la référence au terme @a t
+ * @param t Le terme à rechercher dans le @a conteneur
+ * @return 1 si le terme @a t a été trouvé dans le terme @a conteneur, 0 sinon
+ */
+int contient_terme(const Terme conteneur, const Terme t);
+
+/** @brief Créé un ensemble d'équation à partir des deux fonctions
+ * en faisant la correspondance entre les paramètres:
+ * f1(a, b, c) = f2(d, e, f) => {a = d, b = e, c = f}
+ * @a fgauche et @a fdroit doivent être des fonctions valides (non nuls)
+ * sous peine d'arrêt du programme.
+ *
+ * @param fgauche La fonction gauche de l'équation
+ * @param fdroit La fonction droite de l'équation
+ * @return Une liste chaînée des équations générées ou NULL si le nombre
+ * de paramètres ne correspond pas
+ */
+Equation *decapsuler_fonctions(const Terme fgauche, const Terme fdroit);
+
+
+/** @brief Compte le nombre de paramètres de la fonction @a t
+ * 
+ * @a t doit être une fonction valide (terme non nul) sous peine d'arrêt
+ * du programme
+ *
+ * @param t La fonction dont on veut compter le nombre de paramètres
+ * @return Le nombre d'arguments de la fonction @a t
+ */
+int compter_arguments(const Terme t);
+
+
+/** @brief Insère l'équation e dans le système des solutions @a s
+ * @a s et @a e doivent être des objets valides (non nuls) sous peine
+ * d'arrêt du programme
+ *
+ * @param s Le système dans lequel seront rangées les solutions
+ * @param e L'équation à ranger
+ */
+void ranger_solutions(Solutions s, const Equation *e);
 
 #endif
 
