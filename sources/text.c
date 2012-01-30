821f3f3b7b6307572d0d7f684e7eb4548d66502f
Ali NAJEM <ali.najem@hotmail.fr>
2012-01-30 13:25:01 -0500

    Implementer quelques fonctions .

--- sources/text.c
+++ sources/text.c
@@ -8,6 +8,11 @@
  */ 
 
 #include "text.h"
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
 
 struct Text {
 	char *data;
@@ -15,3 +20,109 @@ struct Text {
 };
 
 
+unsigned text_obtenir_taille(const Text t)
+{
+	assert(t != NULL);
+	return t->length;
+}
+
+Text text_creer_depuis_texte(const char *string)
+{
+	assert(string != NULL);
+	Text texte = (Text)malloc(sizeof(*texte));
+	if (texte == NULL)
+	{
+		fprintf(stderr, "malloc error\n");
+		abort();
+	}
+	else
+	{
+		texte->data = strdup(string);
+		texte->length = strlen(string);
+		return texte;
+	} 
+}
+
+void text_destroy(Text t)
+{
+	assert(t!=NULL);
+	free (t->data);
+	free (t);
+}
+
+const char *text_obtenir_texte(const Text t)
+{
+	assert(t!=NULL);
+	assert(t->data!=NULL);
+	return t->data;
+}
+
+void text_retirer_espaces(Text t)
+{	
+	assert(t!=NULL);
+	assert(t->data!=NULL);
+	
+	int j,i,n;
+	
+	n=strlen(t->data);
+
+	for(i=0;i<n;i++)
+	{
+		if(t->data[i]==' ')
+		{
+			for(j=i;j<n;j++)
+			{
+				t->data[j]=t->data[j+1];
+			}
+		}
+	}
+}
+
+void text_decoupe_premier_niveau(const Text t, char separator, Text **output, unsigned *count)
+{
+	assert(t!=NULL);
+	assert(t->data!=NULL);
+	
+	/*unsigned i;
+	for (i = 0; i < 9; i++)
+		output[i]= NULL;
+	*/
+	
+	int i,n,compteur_parenthese=0,compteur_caractere=0;
+	n=strlen(t->data);
+	*count =0;
+	
+	for(i=0;i<n; i++)
+	{
+		compteur_caractere++;
+		
+		if(t->data[i]=='(')
+		{
+			compteur_parenthese++;
+		}
+		else
+		{
+			if(t->data[i]==')')
+			{
+				compteur_parenthese--;
+			}
+			else
+			{
+				if(t->data[i]==separator)
+				{
+					if(compteur_parenthese ==0)
+					{
+						// Inserer le texte dans le tableau des textes..
+						output[i]->data = &(t->data[i]);
+						output[i]->length = compteur_caractere;
+						t->data = &t->data[i];
+						
+						count++;
+						compteur_caractere=0;
+					}
+					//Sinon on fait rien
+				}
+			}
+		}
+	}
+}
