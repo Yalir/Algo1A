14e7ef06ce4e6af023eff18060e30cbad4c2ca3a
Lucas Soltic <soltic.lucas@gmail.com>
2012-01-30 15:17:39 +0100

    Import initial.

--- sources/parser.h
+++ sources/parser.h
@@ -10,5 +10,56 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include "Types.h"
+#include "text.h"
+
+/** Créé un système d'équations à partir du texte donné
+ * 
+ * @param input Le texte à évaluer
+ * @return Le système généré à partir du texte, ou NULL si une erreur
+ * est survenue
+ */
+Systeme creer_systeme_depuis_texte(const char *input);
+
+
+/** @brief Traite une équation de la forme "...=..." contenue dans @a item.
+ * La fonction considère que @a item ne contient aucun espace.
+ *
+ * @param item Le Text d'où doit être extrait l'équation
+ * @return L'équation générée à partir du Text @a item,
+ * ou NULL si une erreur est survenue
+ */
+Equation *parse_equation(Text item);
+
+
+/** @brief Traite une liste d'arguments de la forme "a,F(b,c),4"
+ * contenue dans @a item.
+ * La fonction considère que @a item ne contient aucun espace.
+ *
+ * @param item Le Text d'où doit être extrait la liste d'arguments
+ * @return La liste d'arguments générée à partir du Text @a item,
+ * ou NULL si une erreur est survenue
+ */
+Argument parse_arguments(Text item);
+
+
+/** @brief Traite un terme de la forme "a" ou "F(b,c)" ou "4" contenu dans @a item.
+ * La fonction considère que @a item ne contient aucun espace.
+ *
+ * @param item Le Text d'où doit être extrait le terme
+ * @return Le terme généré à partir du Text @a item,
+ * ou NULL si une erreur est survenue
+ */
+Terme parse_terme(Text item);
+
+
+/** @brief Traite une fonction de la forme "F(a,F(b,c))" contenue dans @a item.
+ * La fonction considère que @a item ne contient aucun espace.
+ *
+ * @param item Le Text d'où doit être extraite la fonction
+ * @return La fonction générée à partir du Text @a item,
+ * ou NULL si une erreur est survenue
+ */
+Terme parse_fonction(Text item);
 
 #endif
