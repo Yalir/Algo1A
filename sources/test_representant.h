e0d60c8212418fa9f45af0b32c976e67e445fd06
Lucas Soltic <soltic.lucas@gmail.com>
2012-02-04 22:50:09 +0100

    Importation de la structure de test de base.

---
+++ sources/test_representant.h
@@ -0,0 +1,16 @@
+/*
+ * ESIL (Ecole Superieure d'Ingenieurs de Luminy, Marseille)
+ * Case 925 - 163, avenue de Luminy
+ * 13288 Marseille CEDEX 9
+ *
+ * Ce fichier est l'oeuvre d'eleves de l'ESIL. Il ne peut etre
+ * reproduit, utilise ou modifie sans l'avis express de ses auteurs.
+ */ 
+
+#ifndef TEST_REPRESENTANT_H
+#define TEST_REPRESENTANT_H
+
+void test_representant(void);
+
+#endif
+
---
+++ sources/test_types.h
@@ -0,0 +1,16 @@
+/*
+ * ESIL (Ecole Superieure d'Ingenieurs de Luminy, Marseille)
+ * Case 925 - 163, avenue de Luminy
+ * 13288 Marseille CEDEX 9
+ *
+ * Ce fichier est l'oeuvre d'eleves de l'ESIL. Il ne peut etre
+ * reproduit, utilise ou modifie sans l'avis express de ses auteurs.
+ */ 
+
+#include "test_types.h"
+
+void test_types(void)
+{
+	
+}
+
